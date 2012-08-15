0c5207b50c0866570bf622c172e37358c7d45461
ubuntu-12 <mperdikeas@ubuntu.(none)>
2012-08-15 18:27:51 +0300

    jsf#004#STAGE_06: added DML SQL model code to populate users and private data databases

---
+++ jsf/004-00-dynamic-entity_manager-STAGE_06/model/user-02-dml.sql
@@ -0,0 +1,6 @@
+INSERT INTO privateuserdata VALUES('emp02-01', 'emp02-01 pers');
+INSERT INTO privateuserdata VALUES('emp02-02', 'emp02-02 pers');
+INSERT INTO privateuserdata VALUES('emp02-03', 'emp02-03 pers');
+INSERT INTO privateuserdata VALUES('emp02-04', 'emp02-04 pers');
+INSERT INTO privateuserdata VALUES('emp02-05', 'emp02-05 pers');
+INSERT INTO privateuserdata VALUES('emp02-06', 'emp02-06 pers');
