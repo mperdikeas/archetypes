bb71674f6e8e975679386c909ddd6a09e245ebe0
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-08-16 17:27:36 +0300

    JSF#005#00: 1st version with listing working

---
+++ jsf/005-00-passing_information_form2form/prj/users-editing-war/src/java/controllers/UserLController.java
@@ -0,0 +1,69 @@
+package gr.neuropublic.neurojsfpilot.customerservice.backingBeans;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
+import javax.faces.bean.ManagedProperty;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import javax.faces.event.ActionEvent;
+import javax.annotation.PostConstruct;
+import javax.naming.InitialContext;
+import java.util.Map;
+import java.util.HashMap;
+import javax.faces.event.ComponentSystemEvent;
+import java.util.Collection;
+import java.util.Date;
+
+import mutil.base.Pair;
+import mutil.base.Triad;
+import mutil.base.ExceptionAdapter;
+
+import entities.*;
+import facades.*;
+
+
+@ManagedBean
+@RequestScoped
+public class UserLController implements Serializable {
+    private final Logger l = Logger.getLogger(this.getClass().getName());
+    private static final String CLASSNAME=UserLController.class.getName();
+
+    @EJB
+    private UserFacade userFacade;
+    public UserFacade getFacade() {
+        return userFacade;
+    }
+
+    private User current;
+    private List<User> items;
+    public User getCurrent() {return current;}
+    public void setCurrent(User current) {
+        this.current = current;
+    }
+
+    public UserLController() {
+    }
+
+    public List<User> getItems() {
+        if (items == null) {
+            l.info("inside "+CLASSNAME+"#getItems()");            
+            items = getFacade().findAll();
+            l.info("**************** "+items.size()+" items returned");
+        }
+        return items;
+    }
+
+}
