3537e4b06761aaaa3e6dff037a1b1132463df355
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-08-16 16:20:02 +0300

    JSF#005#00: finished the model

---
+++ jsf/005-00-passing_information_form2form/model/jsf005db-dml.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.user VALUES(1, 'John'  , 'Smith'    , 33);
+INSERT INTO public.user VALUES(2, 'Mike'  , 'Partridge', 23);
+INSERT INTO public.user VALUES(3, 'George', 'Archer'   , 83);
+INSERT INTO public.user VALUES(4, 'Peter' , 'Thatcher' , 10);
