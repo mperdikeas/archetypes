8d53d1ee9644ad02c59c814f8c8bcb890a73310c
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-08-27 19:51:22 +0300

    JPQL # 000 # 00 : 1st version of Java SE JPQL application that exits without breaking

---
+++ jpql/000-00-console_application/src/Test.java
@@ -0,0 +1,29 @@
+import mutil.jpapersutil.JPQLUtil;
+import mutil.jpapersutil.QualifiedResultList;
+import javax.persistence.Persistence;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import mutil.base.Pair;
+
+import java.util.Map;
+import java.util.HashMap;
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("foo");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("league");
+        EntityManager em = factory.createEntityManager();
+
+        //     public static <T> QualifiedResultList<T> getResults(EntityManager em, Pair<String, Map<String, Object>> jpql_and_params, Integer recordLimit) {
+
+        String query1 = "SELECT c FROM Customer c";
+        String query2 = "SELECT c FROM Customer c WHERE c.id = :id";
+        String query3 = "SELECT c FROM Customer c WHERE c.name = :name";
+        String query4 = "SELECT c FROM Customer c WHERE c.surname = :surname";
+        String query5 = "SELECT c FROM Customer c WHERE c.employee = :employee";
+        String query6 = "SELECT c FROM Customer c WHERE c.dateOfBirth = :dateOfBirth";
+        String query7 = "SELECT c FROM Customer c WHERE c.streetNo = :streetNo";
+        Pair<String, Map<String, Object>> jpql_and_params = Pair.create(query1, (Map<String, Object>) new HashMap<String,Object>());
+        QualifiedResultList<?> result = JPQLUtil.getResults(em , jpql_and_params , 10);
+    }
+}
