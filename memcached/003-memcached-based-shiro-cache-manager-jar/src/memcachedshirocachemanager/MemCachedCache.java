6029a8c971eb443aca21110ed9c34bed657fc0f1
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-09-05 17:13:27 +0300

    MEMCACHED # 003 : USE CASE INITIAL

---
+++ memcached/003-memcached-based-shiro-cache-manager-jar/src/memcachedshirocachemanager/MemCachedCache.java
@@ -0,0 +1,127 @@
+package memcachedshirocachemanager;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Collection;
+import java.net.InetSocketAddress;
+
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.internal.OperationFuture;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+
+
+public class MemCachedCache<K,V> implements Cache<K,V> {
+
+    private final MemcachedClient memcachedClient;
+    private final String shiroCacheName;
+    private final int expiry = 3600;
+    private final int keysExpiry = 3*24*3600;
+    private Set<K> keys;
+
+    public MemCachedCache(MemcachedClient memcachedClient, String shiroCacheName) {
+        this.memcachedClient = memcachedClient;
+        this.shiroCacheName  = shiroCacheName;
+        keys = getKeys();
+    }
+
+
+    private Set<K> getKeys() {
+        Set<K> retValue = (Set<K>) memcachedClient.get(shiroCacheName);
+        System.out.format("\nretrieved the set %s from memcache under key '%s'\n", retValue, shiroCacheName);
+        return retValue;
+    }
+
+    private void synchKeys() {
+        OperationFuture<Boolean> result = memcachedClient.set(shiroCacheName, keysExpiry, keys);
+        try {
+            if (!result.get()) throw new CacheException();
+        } catch (Exception e) {
+            throw new CacheException(e.getMessage());
+        }
+    }
+
+    private void addToKeys(K key) {
+        System.out.format("asked to add key: %s to the list of keys", key);
+        this.keys = getKeys();
+        if (this.keys==null)
+            this.keys = new TreeSet<K>();
+        this.keys.add(key);
+        synchKeys();
+    }
+
+    private void removeFromKeys(K key) {
+        System.out.format("asked to remove key: %s from the list of keys", key);
+        this.keys = getKeys();
+        if (this.keys==null)
+            this.keys = new TreeSet<K>();
+        this.keys.remove(key);
+        synchKeys();
+    }
+
+    private void clearKeys() {
+        this.keys = new TreeSet<K>();
+        synchKeys();
+    }
+
+
+    public void clear() {
+        this.keys = getKeys();
+        if (keys != null) 
+            for (K key : keys)
+                memcachedClient.delete(memCacheKey(key));
+        clearKeys();
+    }
+
+    public V get(K key) {
+        return (V) memcachedClient.get(memCacheKey(key));
+    }
+
+    public Set<K> keys() {
+        this.keys = getKeys();
+        return (Set<K>) keys;
+    }
+
+    private String memCacheKey(K shiroKey) {
+        return shiroCacheName+String.valueOf(shiroKey.hashCode());
+    }
+
+    // according to specs: returns the previous value associated with the given key or null if there was no previous value 
+    public V put(K key, V value) {
+        String memCacheKey = memCacheKey(key);
+        V prevValue = (V) memcachedClient.get(memCacheKey);
+        OperationFuture<Boolean> result = memcachedClient.set(memCacheKey, expiry, value);
+        try {
+            if (!result.get()) throw new CacheException(String.format("failed to set key / value pair = %s / %s (original key=%s)", key, value, memCacheKey));
+        } catch (Exception e) { throw new CacheException(e.getMessage()); }
+        addToKeys(key);
+        return prevValue;
+    }
+
+    public V remove(K key) {
+        V prevValue = (V) memcachedClient.get(memCacheKey(key));
+        OperationFuture<Boolean> removeResult = memcachedClient.delete(memCacheKey(key));
+        try {
+            if (!removeResult.get()) throw new CacheException("failed to remove key: "+key);
+        } catch (Exception e) { throw new CacheException(e.getMessage()); }
+        removeFromKeys(key);
+        return prevValue;
+    }
+
+    public int size() {
+        this.keys = getKeys();
+        int retValue = this.keys.size();
+        System.out.format("size is %d\n", retValue);
+        return retValue;
+    }
+
+    public Collection<V> values() {
+        this.keys = getKeys();
+        Set<V> retValue = new TreeSet<V>();
+        if (this.keys != null)
+            for (K key : this.keys)
+                retValue.add(get(key));
+        return retValue;
+    }
+
+}
---
+++ memcached/003-memcached-based-shiro-cache-manager-jar/src/memcachedshirocachemanager/ShiroMemCacheConsoleClient.java
@@ -0,0 +1,103 @@
+package memcachedshirocachemanager;
+
+import java.net.InetSocketAddress;
+import java.io.Console;
+import java.util.Set;
+import java.util.Collection;
+
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.cache.Cache;
+
+public class ShiroMemCacheConsoleClient {
+    private static MemCachedCacheManager cacheManager ;
+    private static Cache c ;
+
+    private static final void printVerbs() {
+        System.out.println("verbs are: use, put, get, remove, clear, keys, size, values");
+    }
+
+    public static void main(String[] args) throws Exception {
+        cacheManager = new MemCachedCacheManager();
+        final Console console = System.console();
+        printVerbs();
+        while (true) {
+            String line = console.readLine();
+            String[] command = line.split(" ");
+            switch (command[0]) {
+            case "use"    : use(command[1]);
+                            break;
+            case "put"    : put(command[1], command[2]);
+                            break;
+            case "get"    : get(command[1]);
+                            break;
+            case "remove" : remove(command[1]);
+                            break;
+            case "clear"  : clear();
+                            break;
+            case "keys"   : keys();
+                            break;
+            case "size"   : size();
+                            break;
+            case "values" : values();
+                            break;
+            default:
+                printVerbs();
+            }
+        }
+    }
+
+    private static void use(String cacheName) {
+        c = cacheManager.getCache(cacheName);
+    }
+
+    private static boolean check() {
+        if (c==null) {
+            System.out.println("cache not set - use 'use' to set it");
+            return false;
+        } else return true;
+    }
+
+    private static void put(String key, String value) {
+        if (!check()) return;
+        String oldValue = (String) c.put(key, value);
+        System.out.println("old value was: "+oldValue);
+    }
+
+    private static void get(String key) {
+        if (!check()) return;
+        System.out.println("value is: "+c.get(key));
+    }
+
+    private static void remove(String key) {
+        if (!check()) return;
+        c.remove(key);
+    }
+
+    private static void clear() {
+        if (!check()) return;
+        c.clear();
+    }
+
+    private static void keys() {
+        if (!check()) return;
+        Set<String> keys = (Set<String>) c.keys();
+        if (keys!=null) {
+            for (String key : keys) 
+                System.out.print(key+" ");
+            System.out.println();
+        } else System.out.println("keyset is null");
+    }
+    
+    private static void size() {
+        if (!check()) return;
+        c.size();
+    }
+
+    private static void values() {
+        if (!check()) return;
+        Collection<String> values = (Collection<String>) c.values();
+        for (String value : values)
+                System.out.print(value+" ");
+        System.out.println();
+    }
+}
