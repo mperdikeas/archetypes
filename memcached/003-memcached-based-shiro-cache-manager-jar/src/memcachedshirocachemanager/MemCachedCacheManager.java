6945ce5725342b2be7f77ecf1591470547fdff54
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-09-05 21:14:08 +0300

    MEMCACHED # 003 : added Factory method

--- memcached/003-memcached-based-shiro-cache-manager-jar/src/memcachedshirocachemanager/MemCachedCacheManager.java
+++ memcached/003-memcached-based-shiro-cache-manager-jar/src/memcachedshirocachemanager/MemCachedCacheManager.java
@@ -16,9 +16,22 @@ public class MemCachedCacheManager implements CacheManager {
     private static int instanceCount = 0 ; 
     private Map<String, Cache> createdCaches = new HashMap<String, Cache>();
 
+    private static MemCachedCacheManager theManager;
+
     public MemCachedCacheManager() throws IOException {
-        if (++instanceCount != 1) throw new RuntimeException("was expecting this to be a singleton");
-        c = new MemcachedClient(new InetSocketAddress("localhost", 11211));        
+        if (++instanceCount != 1) throw new RuntimeException("was expecting this to be a singleton"); /* line A  */
+        c = new MemcachedClient(new InetSocketAddress("localhost", 11211));
+        this.theManager = this;
+    }
+    // NB: with regard to the above constructor and the below static factory method
+    // Actually, we need both: Shiro initialization expects a no-arg constructor
+    // but in case my own code needs to use a MemCacheCacheManager for its own inscrutable
+    // purposes we also need the factory method to enforce the singleton.
+    public static MemCachedCacheManager createMemCachedCacheManager() throws IOException {
+        if (theManager==null) {
+            theManager = new MemCachedCacheManager();
+        }
+        return theManager;
     }
 
     public <K,V> Cache<K,V> getCache(String name) {
