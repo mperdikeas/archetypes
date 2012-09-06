43c7b105859ddacdc7f4e49a447b625033757be4
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-09-06 20:31:28 +0300

    SHIRO # 003 # 01 : connects to the Postgresql database but fails to use customized Strings

--- shiro/003-01-customAuthDatabase/webapp/sso-caching-war/src/java/shirorealm/HardcodedJdbcRealm.java
+++ shiro/003-01-customAuthDatabase/webapp/sso-caching-war/src/java/shirorealm/HardcodedJdbcRealm.java
@@ -7,20 +7,31 @@ import org.apache.shiro.realm.jdbc.JdbcRealm;
 
 public class HardcodedJdbcRealm extends JdbcRealm {
 
+    protected static final String DEFAULT_AUTHENTICATION_QUERY =
+    "SELECT a.\"USER_PASSWORD\" FROM \"SS_USERS\" a WHERE a.\"USER_NAME\" = ?"; 
+
+    protected static final String DEFAULT_PERMISSIONS_QUERY =
+    "SELECT c.\"SYEL_DESCRIPTION\" FROM \"SS_ROLES\"              a                                    INNER JOIN "+
+    "                                   \"SS_ROLE_ACCESS_RIGHTS\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
+    "                                   \"SS_SYSTEM_ELEMENTS\"    c ON (b.\"SYEL_ID\" = c.\"SYEL_ID\")            "+
+    " WHERE a.\"ROLE_CAPTION\" = ?                                                                                 ";
+    
+    protected static final String DEFAULT_USER_ROLES_QUERY = 
+    "SELECT a.\"ROLE_CAPTION\" FROM \"SS_ROLES\"      a                                    INNER JOIN "+
+    "                               \"SS_USER_ROLES\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
+    "                               \"SS_USERS\"      c ON (b.\"USER_ID\" = c.\"USER_ID\")            "+
+    "WHERE c.\"USER_NAME\" = ?                                                                        ";
+
+
     public HardcodedJdbcRealm() {
         super();
-        //get the DataSource that JSecurity's JdbcRealm
-        //should use to find the user's password
-        //using the provided username
-        //see context.xml for this DataSource's properties
         InitialContext ic;
         DataSource dataSource;
         try {
             System.out.println("1");
             ic = new InitialContext();
             System.out.println("2");
-            //        dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/security");
-            dataSource = (DataSource) ic.lookup("java:/jsf005db");
+            dataSource = (DataSource) ic.lookup("jdbc/auth");
             System.out.println("3");
             System.out.println("data source is: "+dataSource);
             this.setDataSource(dataSource);
