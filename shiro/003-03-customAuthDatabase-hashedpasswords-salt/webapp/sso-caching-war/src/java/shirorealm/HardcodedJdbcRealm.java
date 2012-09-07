19a6935cd6aaad3365b271657ea8e8ce1f987f69
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-09-07 18:15:01 +0300

    SHIRO # 003 # 03 : replaced authentication query - fails at password checking

--- shiro/003-03-customAuthDatabase-hashedpasswords-salt/webapp/sso-caching-war/src/java/shirorealm/HardcodedJdbcRealm.java
+++ shiro/003-03-customAuthDatabase-hashedpasswords-salt/webapp/sso-caching-war/src/java/shirorealm/HardcodedJdbcRealm.java
@@ -7,16 +7,20 @@ import org.apache.shiro.realm.jdbc.JdbcRealm;
 
 public class HardcodedJdbcRealm extends JdbcRealm {
 
-    protected static final String DEFAULT_AUTHENTICATION_QUERY =
-    "SELECT a.\"USER_PASSWORD\" FROM \"SS_USERS\" a WHERE a.\"USER_NAME\" = ?"; 
+    // we're using a salted realm so we're no longer using the default query
+    /*    protected static final String DEFAULT_AUTHENTICATION_QUERY =
+          "SELECT a.\"USER_PASSWORD\" FROM \"SS_USERS\" a WHERE a.\"USER_NAME\" = ?";  */
 
-    protected static final String DEFAULT_PERMISSIONS_QUERY =
+    protected static final String OVERRIDEN_SALTED_AUTHENTICATION_QUERY =
+    "SELECT a.\"USER_PASSWORD\", a.\"USER_PASSWORD_SALT\" FROM \"SS_USERS\" a WHERE a.\"USER_NAME\" = ?"; 
+
+    protected static final String OVERRIDEN_PERMISSIONS_QUERY =
     "SELECT c.\"SYEL_DESCRIPTION\" FROM \"SS_ROLES\"              a                                    INNER JOIN "+
     "                                   \"SS_ROLE_ACCESS_RIGHTS\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
     "                                   \"SS_SYSTEM_ELEMENTS\"    c ON (b.\"SYEL_ID\" = c.\"SYEL_ID\")            "+
-    " WHERE a.\"ROLE_CAPTION\" = ?                                                                                 ";
+    " WHERE a.\"ROLE_CAPTION\" = ?                                                                                ";
     
-    protected static final String DEFAULT_USER_ROLES_QUERY = 
+    protected static final String OVERRIDEN_USER_ROLES_QUERY = 
     "SELECT a.\"ROLE_CAPTION\" FROM \"SS_ROLES\"      a                                    INNER JOIN "+
     "                               \"SS_USER_ROLES\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
     "                               \"SS_USERS\"      c ON (b.\"USER_ID\" = c.\"USER_ID\")            "+
@@ -36,14 +40,14 @@ public class HardcodedJdbcRealm extends JdbcRealm {
             System.out.println("data source is: "+dataSource);
             this.setDataSource(dataSource);
             System.out.println("4");
-            this.setSaltStyle(JdbcRealm.SaltStyle.NO_SALT);
+            this.setSaltStyle(JdbcRealm.SaltStyle.COLUMN);
         } catch (NamingException e) {
             e.printStackTrace();
             throw new RuntimeException(); // if we can't find the source, we should panic
         }
-        setAuthenticationQuery(DEFAULT_AUTHENTICATION_QUERY);
-        setUserRolesQuery(DEFAULT_USER_ROLES_QUERY);
-        setPermissionsQuery(DEFAULT_PERMISSIONS_QUERY);
+        setAuthenticationQuery(OVERRIDEN_SALTED_AUTHENTICATION_QUERY);
+        setUserRolesQuery     (OVERRIDEN_USER_ROLES_QUERY           );
+        setPermissionsQuery   (OVERRIDEN_PERMISSIONS_QUERY          );
     }
 }
 
