64c4cc5fb02dbf4f736e5af74d62e43925d1b420
Menelaos Perdikeas <mperdikeas@mperdikeas.(none)>
2012-09-07 12:22:17 +0300

    SHIRO # 003 # 02 : STAGE INITIAL

---
+++ shiro/003-02-customAuthDatabase-hashedpasswords/webapp/sso-caching-war/src/java/backingbeans/UpdateEhCacheController.java
@@ -0,0 +1,83 @@
+package backingbeans;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
+import javax.faces.bean.ManagedProperty;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import javax.faces.event.ActionEvent;
+import javax.annotation.PostConstruct;
+import javax.naming.InitialContext;
+import java.util.Map;
+import java.util.HashMap;
+import javax.faces.event.ComponentSystemEvent;
+import java.util.Collection;
+import java.util.Date;
+import javax.servlet.http.HttpSession;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import translation.TranslationCache;
+
+@ManagedBean
+@ViewScoped
+public class UpdateEhCacheController implements Serializable {
+    private static final Logger l = Logger.getLogger(UpdateEhCacheController.class.getName());
+    
+    // private static final transient TranslationCache tc = TranslationCache.getTranslationCache();
+
+    private String newWord;
+    
+    public String getNewWord() {
+        return newWord;
+    }
+    
+    public void setNewWord(String newWord) {
+       this.newWord = newWord;
+    }
+
+    private String translation;
+    
+    public String getTranslation() {
+        return translation;
+    }
+    
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.newWord = getFlashContent();
+    }
+
+    public String getFlashContent() {
+        return (String) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("newWord");
+    }
+
+    public String updateCache() {
+        l.info("flash content is:"        +getFlashContent());
+        l.info("provided translation is: "+translation);
+        TranslationCache.getTranslationCache().putTranslation(newWord, translation);
+        return "goToEhCacheTest";
+    }
+}
+
---
+++ shiro/003-02-customAuthDatabase-hashedpasswords/webapp/sso-caching-war/src/java/shirorealm/HardcodedJdbcRealm.java
@@ -0,0 +1,48 @@
+package shirorealm;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+
+public class HardcodedJdbcRealm extends JdbcRealm {
+
+    protected static final String DEFAULT_AUTHENTICATION_QUERY =
+    "SELECT a.\"USER_PASSWORD\" FROM \"SS_USERS\" a WHERE a.\"USER_NAME\" = ?"; 
+
+    protected static final String DEFAULT_PERMISSIONS_QUERY =
+    "SELECT c.\"SYEL_DESCRIPTION\" FROM \"SS_ROLES\"              a                                    INNER JOIN "+
+    "                                   \"SS_ROLE_ACCESS_RIGHTS\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
+    "                                   \"SS_SYSTEM_ELEMENTS\"    c ON (b.\"SYEL_ID\" = c.\"SYEL_ID\")            "+
+    " WHERE a.\"ROLE_CAPTION\" = ?                                                                                 ";
+    
+    protected static final String DEFAULT_USER_ROLES_QUERY = 
+    "SELECT a.\"ROLE_CAPTION\" FROM \"SS_ROLES\"      a                                    INNER JOIN "+
+    "                               \"SS_USER_ROLES\" b ON (a.\"ROLE_ID\" = b.\"ROLE_ID\") INNER JOIN "+
+    "                               \"SS_USERS\"      c ON (b.\"USER_ID\" = c.\"USER_ID\")            "+
+    "WHERE c.\"USER_NAME\" = ?                                                                        ";
+
+
+    public HardcodedJdbcRealm() {
+        super();
+        InitialContext ic;
+        DataSource dataSource;
+        try {
+            System.out.println("1");
+            ic = new InitialContext();
+            System.out.println("2");
+            dataSource = (DataSource) ic.lookup("jdbc/auth");
+            System.out.println("3");
+            System.out.println("data source is: "+dataSource);
+            this.setDataSource(dataSource);
+            System.out.println("4");
+        } catch (NamingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(); // if we can't find the source, we should panic
+        }
+        setAuthenticationQuery(DEFAULT_AUTHENTICATION_QUERY);
+        setUserRolesQuery(DEFAULT_USER_ROLES_QUERY);
+        setPermissionsQuery(DEFAULT_PERMISSIONS_QUERY);
+    }
+}
+
