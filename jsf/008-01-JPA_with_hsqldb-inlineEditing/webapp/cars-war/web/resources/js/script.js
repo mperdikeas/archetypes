25f5c14705936632d0534156998ac3788c5a113a
mperdikeas <mperdikeas@gmail.com>
2012-09-14 18:56:01 +0300

    JSF # 008 # 01 : more jQuery RETURN tricks

--- jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
+++ jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
@@ -8,10 +8,20 @@ function focusToNextInput(event, element) {
     return true;
 }
 
-//TO DO FOCUS TO NEW ROW
+//TODO: FOCUS TO NEW ROW
 function createNewRow(event) {
     if(event.keyCode==ENTER_KEY_CODE){
         $("#CAR-form\\:BtnAdd").click();
+    return false;
+    }
+return true;
+}
+
+
+
+function hitEnter(event) {
+    if(event.keyCode==ENTER_KEY_CODE){
+        $("#newItem\\:enter").click();
     return false;
     }
 return true;
