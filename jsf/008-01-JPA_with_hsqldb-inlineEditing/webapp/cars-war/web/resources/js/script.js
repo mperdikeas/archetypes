f404456a1c01f226f724051247f5b895b076e351
mperdikeas <mperdikeas@gmail.com>
2012-09-14 16:47:13 +0300

    JSF # 008 # 01 : add new row now works with RETURN (however I would like that to happen only when on the last row)

--- jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
+++ jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
@@ -11,7 +11,7 @@ function focusToNextInput(event, element) {
 //TO DO FOCUS TO NEW ROW
 function createNewRow(event) {
     if(event.keyCode==ENTER_KEY_CODE){
-        $("$CAR-form:BtnAdd").click();
+        $("#CAR-form\\:BtnAdd").click();
     return false;
     }
 return true;
