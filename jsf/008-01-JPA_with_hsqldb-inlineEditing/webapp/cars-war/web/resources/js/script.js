dc5ab4b043d36afd69e700e32ebc3db2c5dc57af
mperdikeas <mperdikeas@gmail.com>
2012-09-14 19:42:16 +0300

    JSF # 008 # 01 : managed to handle F8 as well

--- jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
+++ jsf/008-01-JPA_with_hsqldb-inlineEditing/webapp/cars-war/web/resources/js/script.js
@@ -1,20 +1,21 @@
 var ENTER_KEY_CODE = 13;
+var F3_KEY_UP      = 114;
 
 function focusToNextInput(event, element) {
     if(event.keyCode==ENTER_KEY_CODE){
         $(":input:eq("+($(":input").index(element)+1)+")").focus();
         return false;
-    }
-    return true;
+    } else
+        return true;
 }
 
 //TODO: FOCUS TO NEW ROW
 function createNewRow(event) {
     if(event.keyCode==ENTER_KEY_CODE){
         $("#CAR-form\\:BtnAdd").click();
-    return false;
-    }
-return true;
+        return false;
+    } else
+        return true;
 }
 
 
@@ -22,8 +23,20 @@ return true;
 function hitEnter(event) {
     if(event.keyCode==ENTER_KEY_CODE){
         $("#newItem\\:enter").click();
-    return false;
-    }
-return true;
+        return false;
+    } else
+        return true;
 }
+
+processKeyEvent = function (eventType, event) {
+    if (event.keyCode==F3_KEY_UP) {
+        $("#CAR-form\\:BtnAdd").click();
+        return false;
+    } else
+        return true;
+}
+
+processKeyUp = function(event) {
+       return processKeyEvent("onkeyup", event);
+};
 
