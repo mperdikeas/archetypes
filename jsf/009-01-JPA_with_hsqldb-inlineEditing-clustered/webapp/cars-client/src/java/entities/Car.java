6de8964f49f75b723cf979347a1bf7c8343b4e89
mperdikeas <mperdikeas@gmail.com>
2012-09-17 13:53:33 +0300

    JSF # 009 # 01 : STAGE INITIAL

---
+++ jsf/009-01-JPA_with_hsqldb-inlineEditing-clustered/webapp/cars-client/src/java/entities/Car.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@Table(name = "CAR")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Car.findAll"    , query = "SELECT x FROM Car x"),
+    @NamedQuery(name = "Car.findByModel", query = "SELECT x FROM Car x WHERE x.model = :MODEL")
+        }) 
+public class Car implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "MODEL")
+    private String model;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRICE")
+    private Integer price;
+
+
+    public Car() {}
+
+    public Car(String model, Integer price) {
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+    public int getHashCode() {
+        return hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (model != null ? model.hashCode() : 0);
+        hash += (price != null ? price.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Car)) {
+            return false;
+        }
+        Car other = (Car) object;
+        if ((this.model == null && other.model != null) || 
+            (this.model != null && !this.model.equals(other.model))) {
+            return false;
+        }
+        if ((this.price == null && other.price != null) || 
+            (this.price != null && !this.price.equals(other.price))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return Car.class.getName()+"[ model =" + model + ", price ="+price+" ]";
+    }
+    
+}
---
+++ jsf/009-01-JPA_with_hsqldb-inlineEditing-clustered/webapp/cars-war/web/resources/js/script.js
@@ -0,0 +1,60 @@
+var ENTER_KEY_CODE   = 13;
+var F3_KEY_UP        = 114;
+var ARROWUP_KEY_UP   = 38;
+var ARROWDOWN_KEY_UP = 40;
+
+var not_yet_focused = true;
+
+function focusInput() { // focusing does not yet work as I need to find a way to
+                        // only focus the very first time the page is loaded and
+                        // rely on arrow key navigation.
+    $('#CAR-form\\:CAR-data-table\\:0\\:modelrow').focus();
+}
+
+function focusToNextInput(event, element) {
+    if(event.keyCode==ENTER_KEY_CODE){
+        $(":input:eq("+($(":input").index(element)+1)+")").focus();
+        return false;
+    } else
+        return true;
+}
+
+//TODO: FOCUS TO NEW ROW
+function createNewRow(event) {
+    if(event.keyCode==ENTER_KEY_CODE){
+        $("#CAR-form\\:BtnAdd").click();
+        return false;
+    } else
+        return true;
+}
+
+
+
+function hitEnter(event) {
+    if(event.keyCode==ENTER_KEY_CODE){
+        $("#newItem\\:enter").click();
+        return false;
+    } else
+        return true; // I am not quite sure what is the import of these return statements; I've experimented but didn't arrive at any results.
+}
+
+processKeyEvent = function (eventType, event) {
+    if (event.keyCode==F3_KEY_UP) {
+        $("#CAR-form\\:BtnAdd").click();
+        return false; 
+    } else
+    if (event.keyCode==ARROWUP_KEY_UP) {
+        $("#CAR-form\\:RowPrev").click();
+        return false; 
+    } else
+    if (event.keyCode==ARROWDOWN_KEY_UP) {
+        $("#CAR-form\\:RowNext").click();
+        return false; 
+    } else
+        return true;
+}
+
+processKeyUp = function(event) {
+       return processKeyEvent("onkeyup", event);
+};
+
