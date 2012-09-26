4ec0380e1d06ad0aa68f89083cfa5eaeaa54bafd
mperdikeas <mperdikeas@gmail.com>
2012-09-26 18:35:27 +0300

    SHIRO # 006 # 00 : 1st working version when I succeeded in Shiro LDAP authentication - NOT authorization

--- shiro/006-00-Shiro-LDAP/src/ShiroTest.java
+++ shiro/006-00-Shiro-LDAP/src/ShiroTest.java
@@ -11,10 +11,10 @@ public class ShiroTest {
     private static Logger logger = LoggerFactory.getLogger(ShiroTest.class);
 
     public static void main(String[] args) {
-        // Using the IniSecurityManagerFactory, which will use the an INI file
-        // as the security file.
-        Factory<org.apache.shiro.mgt.SecurityManager> factory = 
-            new IniSecurityManagerFactory("auth.ini");
+
+        // Using the IniSecurityManagerFactory, which will use the
+        // activated INI file as the security file.
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("activated.ini");
 
         // Setting up the SecurityManager...
         org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
@@ -23,11 +23,12 @@ public class ShiroTest {
         Subject user = SecurityUtils.getSubject();
 
         logger.info("User is authenticated:  " + user.isAuthenticated());
-        
-        UsernamePasswordToken token = new UsernamePasswordToken("bjangles", "dance");
-        
+
+        UsernamePasswordToken token = 
+            new UsernamePasswordToken("cn=orcladmin,cn=Users,dc=neuropublic,dc=gr", "welcome1");
+
         user.login(token);
-        
+
         logger.info("User is authenticated:  " + user.isAuthenticated());
     }
 }
