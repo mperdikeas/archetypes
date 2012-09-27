5b4e4fe38b7101fcd719b0398173aed904f1a26e
mperdikeas <mperdikeas@gmail.com>
2012-09-27 16:48:20 +0300

    UNBOUNDID # 000 # 00 : USE CASE AND STAGE INITIAL (dead-simple login)

---
+++ unboundid/000-00-ldap-server-login/src/UnboundIdLoginTest.java
@@ -0,0 +1,12 @@
+import com.unboundid.ldap.sdk.LDAPConnection;
+
+public class UnboundIdLoginTest {
+    public static void main(String[] args) throws Exception {
+        String host   = "172.31.128.30";
+        int    port   = 389;
+        String bindDN = "cn=orcladmin,cn=Users,dc=neuropublic,dc=gr";
+        String passwd = "welcome1";
+        LDAPConnection conn = new LDAPConnection(host, port, bindDN, passwd);
+    }
+}
+
