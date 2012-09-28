dcadefda80d77155e0a626ed1685e86b97301750
mperdikeas <mperdikeas@gmail.com>
2012-09-28 10:40:44 +0300

    UNBOUNDID # 000 # 01 : STAGE TERMINAL

--- unboundid/000-01-ldap-operations/src/UnboundIdOperationsTest.java
+++ unboundid/000-01-ldap-operations/src/UnboundIdOperationsTest.java
@@ -9,7 +9,10 @@ public class UnboundIdOperationsTest {
         String passwd = "welcome1";
         LDAPConnection conn = new LDAPConnection(host, port, bindDN, passwd);
         System.out.println("connection established");
+        System.out.println("/*************** USERS ***************\\");
         printUsers(conn);
+        System.out.println("/*************** GROUPS ***************\\");
+        printGroups(conn);
         conn.close();
         System.out.println("connection closed");
     }
@@ -18,11 +21,22 @@ public class UnboundIdOperationsTest {
         Filter filter = Filter.createEqualityFilter("objectclass", "person");
         // possible SearchScope values include BASE, ONE, SUB and SUBORDINATE_SUBTREE
         SearchRequest searchRequest = new SearchRequest("dc=neuropublic,dc=gr", SearchScope.SUB, filter, "cn");
+        printNames(conn, searchRequest, "cn");
+    }
+
+    private static void printGroups(LDAPConnection conn) {
+        Filter filter = Filter.createEqualityFilter("objectclass", "orclGroup");
+        // possible SearchScope values include BASE, ONE, SUB and SUBORDINATE_SUBTREE
+        SearchRequest searchRequest = new SearchRequest("dc=neuropublic,dc=gr", SearchScope.SUB, filter, "cn");
+        printNames(conn, searchRequest, "cn");
+    }
+
+    private static void printNames(LDAPConnection conn, SearchRequest searchRequest, String attributeName) {
         try {
             SearchResult searchResult = conn.search(searchRequest);
             System.out.println(searchResult.getEntryCount()+" matching entries returned");
             for (SearchResultEntry entry : searchResult.getSearchEntries()) {
-                String name = entry.getAttributeValue("cn");
+                String name = entry.getAttributeValue(attributeName);
                 System.out.println("name is :"+name);
             }
         } catch (LDAPSearchException lse) {
