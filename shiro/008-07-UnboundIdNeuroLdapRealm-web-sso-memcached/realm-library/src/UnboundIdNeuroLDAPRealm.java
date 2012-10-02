9de862b4291948c83a2e9de685f00aef92ead88c
mperdikeas <mperdikeas@gmail.com>
2012-10-02 12:28:52 +0300

    SHIRO # 008 # 07 : STAGE TERMINAL (managed to do Shiro/SSO against LDAP using MemCached)

---
+++ shiro/008-07-UnboundIdNeuroLdapRealm-web-sso-memcached/realm-library/src/UnboundIdNeuroLDAPRealm.java
@@ -0,0 +1,276 @@
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken; 
+import org.apache.shiro.authz.AuthorizationException;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import javax.naming.NamingException;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.LDAPSearchException;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.SearchScope;
+import com.unboundid.ldap.sdk.SearchRequest;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.Filter;
+import com.unboundid.ldap.sdk.Attribute;
+
+import mutil.base.Pair;
+
+public class UnboundIdNeuroLDAPRealm extends AuthorizingRealm {
+
+    /*--------------------------------------------
+    |             C O N S T A N T S             |
+    ============================================*/
+
+    private static final Logger log = LoggerFactory.getLogger(UnboundIdNeuroLDAPRealm.class);
+
+    /*-------------------------------------------
+    |    I N S T A N C E   V A R I A B L E S    |
+    ============================================*/
+    private String  principalSuffix                      = null;
+    private String  principalRDTag                       = null;
+    private String  searchBase                           = null;
+    private String  host                                 = null;
+    private String  systemUserDN                         = null;
+    private String  systemPasswd                         = null;
+    private boolean reportRolesAndPrivilligesWithRDOnly  = false;
+    private String rolesAndPrivilligesSuffix            = null;
+    private String rolesAndPrivilligesRDTag             = null;
+    private ILdapConnectionFactory ldapConnectionFactory= null;
+
+    /*-------------------------------------------
+    |         C O N S T R U C T O R S           |
+    ============================================*/
+
+    /*-------------------------------------------
+    |  A C C E S S O R S / M O D I F I E R S    |
+    ============================================*/
+    public void setPrincipalSuffix                     (String  principalSuffix) { this.principalSuffix = principalSuffix ; }
+    public void setPrincipalRDTag                      (String  principalRDTag ) { this.principalRDTag = principalRDTag   ; }
+    public void setSearchBase                          (String  searchBase     ) { this.searchBase = searchBase           ; }
+    public void setHost                                (String  host           ) { this.host = host                       ; }
+    public void setSystemUserDN                        (String  systemUserDN   ) { this.systemUserDN = systemUserDN       ; }
+    public void setSystemPasswd                        (String  systemPasswd   ) { this.systemPasswd = systemPasswd       ; }
+    public void setReportRolesAndPrivilligesWithRDOnly (boolean boolValue      ) { this.reportRolesAndPrivilligesWithRDOnly = boolValue ; }
+    public void setRolesAndPrivilligesSuffix           (String  strValue       ) { this.rolesAndPrivilligesSuffix           = strValue  ; }
+    public void setRolesAndPrivilligesRDTag            (String  strValue       ) { this.rolesAndPrivilligesRDTag             = strValue  ; }
+    public void setLDAPConnectionFactory(ILdapConnectionFactory ldapConnectionFactory) { 
+                                                                   this.ldapConnectionFactory = ldapConnectionFactory;
+    }
+
+    /*--------------------------------------------
+    |               M E T H O D S                |
+    ============================================*/
+
+    protected void onInit() {
+        super.onInit();
+        ensureLDAPConnectionFactory();
+    }
+
+    private ILdapConnectionFactory ensureLDAPConnectionFactory() {
+        if (this.ldapConnectionFactory == null) {
+            log.debug("No LdapConnectionFactory specified - creating a default instance.");
+            ILdapConnectionFactory defaultFactory = new DefaultLdapConnectionFactory();
+            defaultFactory.setHost           (this.host);
+            defaultFactory.setSystemUserDN   (this.systemUserDN);
+            defaultFactory.setSystemPasswd   (this.systemPasswd);
+            this.ldapConnectionFactory = defaultFactory;
+        }
+        return this.ldapConnectionFactory;
+    }
+
+
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        try {
+            return queryForAuthenticationInfo(token, ensureLDAPConnectionFactory());
+        } catch (LDAPException lde) {
+            throw new AuthenticationException("LDAP Authentication over UnboundId failed", lde);
+        }
+    }
+
+    protected AuthenticationInfo queryForAuthenticationInfo(AuthenticationToken token, ILdapConnectionFactory ldapConnectionFactory)
+            throws LDAPException {
+        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+        String username   = upToken.getUsername();
+        String usernameDN = dnFromUsername(username);
+        log.info(String.format("checking for authentication of '%s'->'%s'", username, usernameDN));
+        ldapConnectionFactory.canGetThrowAwayLDAPConnection(usernameDN, String.valueOf(upToken.getPassword()));
+        return new SimpleAuthenticationInfo(username, upToken.getPassword(), getName());
+    }
+
+    private String dnFromUsername(String username) {
+        String usernameDN = String.format("%s=%s,%s", principalRDTag, username, principalSuffix);
+        log.info(String.format("translated: '%s' to: '%s'", username, usernameDN));
+        return usernameDN;
+    }
+
+
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        AuthorizationInfo info;
+        try {
+            info = queryForAuthorizationInfo(principals, ensureLDAPConnectionFactory());
+        } catch (LDAPException e) {
+            String msg = "LDAP Exception: Authorization over UnboundId failed while attempting to retrieve authorization for user [" + principals + "].";
+            throw new AuthorizationException(msg, e);
+        }
+        return info;
+    }
+
+
+    protected AuthorizationInfo queryForAuthorizationInfo(PrincipalCollection principals, ILdapConnectionFactory ldapConnectionFactory)
+        throws LDAPException, LDAPSearchException {
+        String username = (String) getAvailablePrincipal(principals);
+        String usernameDN = dnFromUsername(username);
+        LDAPConnection ldapConn = ldapConnectionFactory.getConnection();
+        Pair<Set<String>, Set<String>> rolesAndPermissions;
+        try {
+            rolesAndPermissions = getRolesAndPermissionsForUser(ldapConn, usernameDN);
+            for (String role : rolesAndPermissions.a)
+                log.info(String.format("user %s has role %s", username, role));
+            for (String role : rolesAndPermissions.b)
+                log.info(String.format("user %s has permission %s", username, role));
+            
+        } finally {
+            ldapConn.close();
+        }
+        SimpleAuthorizationInfo retValue = new SimpleAuthorizationInfo(rolesAndPermissions.a);
+        {
+            Set<String> permissions = rolesAndPermissions.b;
+            for (String permission : permissions)
+                log.info(String.format("giving persmission %s to user %s", permission, username));
+            retValue.addStringPermissions(rolesAndPermissions.b);
+        }
+        return retValue;
+        // TODO: will have to recursively add groups for permissions as well
+        // TODO: will have to replace the DN names of groups with a mapped role and privillige name by dropping the suffix
+    }
+
+
+    private Pair<Set<String>, Set<String>> getRolesAndPermissionsForUser(LDAPConnection ldapConn, String usernameDN) throws LDAPException {
+        // 1st implementation: we don't translate the groups into roles in any manner
+        // or do any sort of clever traversal routine to distinguish between roles (i.e. groups having another group 
+        // as a child) and permissions (i.e. groups not having another group as a child)
+        // return getGroupsWithUser(ldapConn, usernameDN);
+        return getRecursiveFatherAndChildlessGroupsOfUser(ldapConn, usernameDN);
+    }
+
+
+    @Deprecated // simplified implementation which I don't use any more
+    private Set<String> getGroupsWithUser(LDAPConnection conn, String userDN) throws LDAPSearchException {
+        Filter both               = Filter.createANDFilter(
+                                        Filter.createEqualityFilter("objectclass", "orclGroup"),
+                                        Filter.createEqualityFilter("uniqueMember", userDN)
+                                    );
+        SearchRequest sr = new SearchRequest(searchBase, SearchScope.SUB, both, "cn");
+
+        SearchResult searchResult = conn.search(sr);
+        log.debug(searchResult.getEntryCount()+" matching entries returned");
+        Set<String> retValue = new LinkedHashSet<String>();
+        for (SearchResultEntry entry : searchResult.getSearchEntries())
+            retValue.add(entry.getDN());
+        return retValue;
+    }
+
+    private Pair<Set<String>, Set<String>> getRecursiveFatherAndChildlessGroupsOfUser(LDAPConnection conn, String userDN)
+        throws LDAPSearchException
+    {
+        Filter both               = Filter.createANDFilter(
+                                        Filter.createEqualityFilter("objectclass", "orclGroup"),
+                                        Filter.createEqualityFilter("uniqueMember", userDN)
+                                    );
+        SearchRequest sr = new SearchRequest(searchBase, SearchScope.SUB, both, "cn");
+
+        SearchResult searchResult = conn.search(sr);
+        log.debug(String.format("User '%s' is a direct member of %d groups", userDN, searchResult.getEntryCount()));
+        Set<String> fatherGroups     = new LinkedHashSet<String>();
+        Set<String> childlessGroups  = new LinkedHashSet<String>();
+        for (SearchResultEntry entry : searchResult.getSearchEntries())
+            recursivelyAddGroups(conn, fatherGroups, childlessGroups, entry.getDN());
+        int numOfFatherGroups     = fatherGroups.size();
+        int numOfChildlessGroups  = childlessGroups.size(); 
+        int numOfDirectGroups     = searchResult.getEntryCount();
+        int numOfIndirectGroups   = numOfFatherGroups + numOfChildlessGroups - numOfDirectGroups;
+        log.info(String.format("User '%s' is direct member of %d groups and indirect member of %d groups; of "+
+                                "these, %d are role-groups and %d are permission-groups"
+                                , userDN, numOfDirectGroups, numOfIndirectGroups, numOfFatherGroups, numOfChildlessGroups));
+        return Pair.create(fatherGroups, childlessGroups);
+    }
+
+    private boolean isGroup(String dn) {
+        String groupsSubtree = "cn=Groups,dc=neuropublic,dc=gr";
+        return dn.toLowerCase().endsWith(groupsSubtree.toLowerCase());
+    }
+
+    private void recursivelyAddGroups(LDAPConnection conn, Set<String> fatherGroups, Set<String> childlessGroups, String groupDN)
+        throws LDAPSearchException
+    {
+        log.info("looking for a group under base: "+groupDN);
+        Filter sillyFilterInLieuOfAnAlwaysTrueFilter = Filter.createPresenceFilter("cn");
+        SearchRequest sr = new SearchRequest(groupDN, SearchScope.BASE, sillyFilterInLieuOfAnAlwaysTrueFilter, "uniquemember");
+        SearchResult searchResult = conn.search(sr);
+        List<SearchResultEntry> searchResultEntries = searchResult.getSearchEntries();
+        if (searchResultEntries.size()!=1) throw new RuntimeException(searchResultEntries.size()+" is not equal to 1");
+        SearchResultEntry entry = searchResultEntries.get(0);
+        Attribute uniqueMembers = entry.getAttribute("uniquemember");
+        if (uniqueMembers != null) {
+            String[] uniqueMembersStrArray = uniqueMembers.getValues();
+            boolean foundOneChildGroup = false;
+            for (String member : uniqueMembersStrArray) {
+                log.info("examining: "+member);
+                if (isGroup(member)) {
+                    log.info(member+" is group");
+                    foundOneChildGroup = true;
+                    recursivelyAddGroups(conn, fatherGroups, childlessGroups, member);
+                }
+            }
+            if (roleOrPrivilligeName(groupDN)!=null) {
+                if (foundOneChildGroup) {
+                    log.info("adding "+groupDN+" as one of the father groups");
+                    fatherGroups.add(roleOrPrivilligeName(groupDN));
+                }
+                else {
+                    log.info("adding "+groupDN+" as one of the childless groups");
+                    childlessGroups.add(roleOrPrivilligeName(groupDN));
+                }
+            }
+        } else {
+            log.info("attribute 'uniquemember' for group '"+groupDN+"' is null - adding the group as a privillige");
+            if (roleOrPrivilligeName(groupDN)!=null) {
+                log.info(String.format("adding privillige %s", roleOrPrivilligeName(groupDN)));
+                childlessGroups.add(roleOrPrivilligeName(groupDN));
+            }
+        }
+    }
+
+    private String extractRDfromDN(String dn) {
+        String firstComponent = dn.split(",")[0];
+        final String ASSUMED_PREFFIX = rolesAndPrivilligesRDTag+"=";
+        if (!firstComponent.startsWith(ASSUMED_PREFFIX)) throw new RuntimeException(dn);
+        return firstComponent.replaceFirst(ASSUMED_PREFFIX, "");
+    }
+
+    private String roleOrPrivilligeName(String groupDN) {
+        if (this.reportRolesAndPrivilligesWithRDOnly) {
+            if (groupDN.toLowerCase().endsWith(rolesAndPrivilligesSuffix.toLowerCase()))
+                return extractRDfromDN(groupDN);
+            else return null;
+        }
+        else return groupDN;
+    }
+
+
+
+}
---
+++ shiro/008-07-UnboundIdNeuroLdapRealm-web-sso-memcached/webapp/sso-caching-war/src/java/translation/TranslationCache.java
@@ -0,0 +1,104 @@
+package translation;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.cache.Cache;
+import memcachedshirocachemanager.MemCachedCacheManager;
+
+public class TranslationCache {
+
+    private static final Logger l = Logger.getLogger(TranslationCache.class.getName());
+
+    private static TranslationCache tc = null;
+    public static TranslationCache getTranslationCache() {
+        l.info(" I am here (TranslationCache#getTranslationCache())" );
+        if (tc == null) {
+            l.info("TranslationCache#getTranslationCache(): tc is null, setting it");
+            tc = new TranslationCache();
+        } else l.info("TranslationCache#TranslationCache(): tc is not null");
+        return tc;
+    }
+
+    private static final TranslationDatabase translationDatabase = new TranslationDatabase();
+
+    private static CacheManager cacheManager;
+    private CacheManager getCacheManager() {
+        try {
+            if (cacheManager==null) {
+                cacheManager = MemCachedCacheManager.createMemCachedCacheManager();
+            }
+            return cacheManager;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+  public String getTranslation(String word) {
+    Object elem = getCache().get(word);
+    if (elem == null) {
+      String translatedWord = translationDatabase.getTranslation(word);
+      if (translatedWord == null) { return null; }
+      getCache().put(word, translatedWord);
+    }
+    return (String) elem;
+  }
+
+    public void putTranslation(String word, String translation) {
+        getCache().put(word, translation);
+        l.info(String.format("just put a new element (%s,%s), into the cache", word, translation));
+    }
+
+  private String getCachedTranslation(String name) {
+    Object elem = getCache().get(name);
+    return (elem != null) ? (String) elem : null;
+  }
+
+    public int getSizeOfTranslationDictionary() {
+        return getOriginalWordsInCache().length;
+    }
+
+  public String[] getOriginalWordsInCache() {
+    @SuppressWarnings("unchecked")
+    List<String> list = new ArrayList<String>();
+    Set<String> keysSet = getCache().keys();
+    if (keysSet != null) {
+        Iterator<String> keys = keysSet.iterator();
+        while (keys.hasNext()) {
+          String name = keys.next();
+          l.info(String.format("checking for the translation of word: '%s'", name));
+          
+          boolean showWeirdCondition = false;
+          if (showWeirdCondition) {
+              if (getCachedTranslation(name) != null) {
+                list.add(name);
+              } else throw new RuntimeException("very rare - wasn't expecting that - race condition?");
+                      // this has only happened to me once with a terracotta server array
+                      // how is it possible that a key is found in the cache without the value ?
+                      // I should investigate into it.
+          }
+          else list.add(name);
+        }
+    }
+    return list.toArray(new String[list.size()]);
+  }
+
+
+
+  public int getSize() {
+      return getCache().size();
+  }
+
+  private Cache getCache() {
+      final String cacheName = "dictionary";
+      Cache retValue = getCacheManager().getCache(cacheName);
+      if (retValue == null) throw new RuntimeException("unable to find cache: "+ cacheName);
+      return retValue;
+  }
+}
