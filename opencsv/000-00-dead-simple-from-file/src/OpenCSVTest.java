87372dfe3d19dd9279b7ca57264de23d7435a404
mperdikeas <mperdikeas@gmail.com>
2012-10-08 10:27:26 +0300

    OPENCSV # 000 # 00 : dead simple example reading from a java.io.File

---
+++ opencsv/000-00-dead-simple-from-file/src/OpenCSVTest.java
@@ -0,0 +1,14 @@
+import au.com.bytecode.opencsv.*;
+import java.io.FileReader;
+public class OpenCSVTest {
+
+
+    public static void main(String args[]) throws Exception {
+        CSVReader reader = new CSVReader(new FileReader("test.csv"));
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            // nextLine[] is an array of values from the line
+            System.out.println(nextLine[0] + " " + nextLine[1] + " " + nextLine[2] + " " + nextLine[3]+ " etc...");
+        }
+    }
+}
