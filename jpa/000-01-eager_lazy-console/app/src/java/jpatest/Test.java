6fac495f3a5b60f6980cbedfabcc8cd4378d1932
mperdikeas <mperdikeas@gmail.com>
2012-10-24 12:47:42 +0300

    JPA # 000 # 01 : STAGE TERMINAL (simplest of JPA examples, just create a new entity)

--- jpa/000-01-eager_lazy-console/app/src/java/jpatest/Test.java
+++ jpa/000-01-eager_lazy-console/app/src/java/jpatest/Test.java
@@ -1,23 +1,27 @@
 package jpatest;
 import javax.persistence.*;
+import entities.A;
+import java.util.logging.Logger;
+
 public class Test {
+    private static final Logger l = Logger.getLogger(Test.class.getName());
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory =  Persistence.createEntityManagerFactory("testjpa");
+        l.info("entity manager factory obtained");
         EntityManager em = entityManagerFactory.createEntityManager();
+        l.info("entity manager obtained");
         EntityTransaction userTransaction = em.getTransaction();
-        
+        l.info("user transaction obtained");
         userTransaction.begin();
-        /*        Customer customer = new Customer();
-        customer.setFirstName("Charles");
-        customer.setLastName("Dickens");
-        customer.setCustType("RETAIL");
-        customer.setStreet("10 Downing Street");
-        customer.setAppt("1");
-        customer.setCity("NewYork");
-        customer.setZipCode("12345");
-        em.persist(customer);*/
+        l.info("transaction begun");
+        A a = new A(2, "a second record");
+        em.persist(a);
+        l.info("object persisted");
         userTransaction.commit();
+        l.info("transaction commited");
         em.close();
+        l.info("entity manager closed");
         entityManagerFactory.close();
+        l.info("entity manager factory closed");
     }
 }
