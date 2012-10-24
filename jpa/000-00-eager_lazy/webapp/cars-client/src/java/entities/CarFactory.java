8f788cc0d524d116e4ba580683b5afdf86b6b30a
mperdikeas <mperdikeas@gmail.com>
2012-10-24 10:20:33 +0300

    JPA # 000 # 00 : USE CASE AND STAGE INITIAL (experimenting with eager / lazy loading)

---
+++ jpa/000-00-eager_lazy/webapp/cars-client/src/java/entities/CarFactory.java
@@ -0,0 +1,88 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@Table(name = "CAR_FACTORY")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "CarFactory.findAll"           , query = "SELECT x FROM CarFactory x"),
+    @NamedQuery(name = "CarFactory.findByModelFactory", query = "SELECT x FROM CarFactory x WHERE x.modelFactory = :MODELFACTORY")
+        }) 
+public class CarFactory implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @NotNull
+    @Embedded
+    @AttributeOverrides({
+                @AttributeOverride(name="model", column=@Column(name="MODEL")),
+                @AttributeOverride(name="factory" , column=@Column(name="FACTORY_NAME"))
+                })
+    private ModelFactory modelFactory;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "FACTORY_DESC")
+    private String factoryDesc;
+
+
+
+
+    public CarFactory() {}
+
+    public CarFactory(ModelFactory modelFactory, String factoryDesc) {
+        this.modelFactory = modelFactory;
+        this.factoryDesc = factoryDesc;
+    }
+
+    public ModelFactory getModelFactory() {
+        return modelFactory;
+    }
+
+    public void setModelFactory(ModelFactory modelFactory) {
+        this.modelFactory = modelFactory;
+    }
+
+    public String getFactoryDesc() {
+        return factoryDesc;
+    }
+
+    public void setFactoryDesc(String factoryDesc) {
+        this.factoryDesc = factoryDesc;
+    }
+    
+    public int getHashCode() {
+        return hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof CarFactory)) {
+            return false;
+        }
+        CarFactory other = (CarFactory) object;
+        if ((this.modelFactory == null && other.modelFactory != null) || 
+            (this.modelFactory != null && !this.modelFactory.equals(other.modelFactory))) {
+            return false;
+        }
+        if ((this.factoryDesc == null && other.factoryDesc != null) || 
+            (this.factoryDesc != null && !this.factoryDesc.equals(other.factoryDesc))) {
+            return false;
+        } 
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return CarFactory.class.getName()+"[ modelFac =" + modelFactory+", factoryDesc = "+factoryDesc+" ]";
+    }
+}
