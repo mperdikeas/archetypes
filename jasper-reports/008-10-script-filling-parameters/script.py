fa52b4496f1748502c4e4f20223831d13f0b53c2
mperdikeas <mperdikeas@gmail.com>
2012-12-05 13:43:32 +0200

    JASPER # 008 # 10 : multiple rows support and print of parameters in order of appearence

---
+++ jasper-reports/008-10-script-filling-parameters/script.py
@@ -0,0 +1,26 @@
+q1 = sql.q('SELECT MIN(USER_ID) FROM SS_USERS')
+STR_1 = 'Πάϊ ένα'
+I_2=3**2
+I_3=2
+I_4=I_2**I_3
+I_5=q1[1]
+q2 = sql.q('SELECT user_nickname FROM SS_USERS WHERE user_id=%d'%I_5)
+q3 = sql.q('SELECT USER_ID FROM SS_USERS WHERE user_nickname=\'%s\''%q2[1])
+STR_b=q2[1]
+STR_C=str(q3[1])
+q4 = sql.q('SELECT user_surname FROM SS_USERS WHERE user_nickname=\''+q2[1]+'\'')
+STR_D = q4[1]
+q5 = sql.q('SELECT user_firstname FROM SS_USERS WHERE user_nickname=\'Βασιλάκης\'')
+STR_D1 = q5[1]
+STR_D1 = str(q5[1])
+#STR_D1 = str(q2[1]) # this throws: UnicodeEncodeError: 'ascii' codec can't encode characters in position 0-8: ordinal not in range(128)
+STR_D1 = str(u.escape(q2[1])) # this works
+q6 = sql.q('SELECT user_email FROM SS_USERS WHERE user_nickname=\''+STR_D1+'\'')
+STR_E=q5[1]
+STR_F=q6[1]
+q7 = sql.qm('SELECT user_nickname FROM SS_USERS', 5, False)
+STR_USER1 = q7[1][1]
+STR_USER2 = q7[2][1]
+STR_USER3 = q7[3][1]
+STR_USER4 = q7[4][1]
+STR_USER5 = q7[5][1]
---
+++ jasper-reports/008-10-script-filling-parameters/src/QueryJythonHolder.java
@@ -0,0 +1,114 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.ResultSetMetaData;
+import java.util.Map;
+import java.util.HashMap;
+
+import mutil.base.ExceptionAdapter;
+import mutil.jdbc.JdbcUtils;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class QueryJythonHolder {
+
+    /*    private String driver;
+    private String connectString;
+    private String user;
+    private String password;
+
+    private QueryJythonHolder(String driver, String connectString, String user, String password) throws ClassNotFoundException {
+        this.driver = driver;
+        this.connectString = connectString;
+        this.user = user;
+        this.password = password;
+        Class.forName(driver);
+        }*/
+
+    private Connection conn;
+    public QueryJythonHolder(Connection conn) {
+        this.conn = conn;
+    }
+
+    /*    private static QueryJythonHolder singleton;
+
+    public static void initializeHolder(String driver, String connectString, String user, String password) {
+        if (singleton != null) throw new RuntimeException();
+        singleton = new QueryJythonHolder(driver, connectString, user, password);
+    }
+
+    public static Map<Integer, Object> q(String query) throws SQLException {
+        return singleton.query(query);
+    }*/
+
+    public String escape(String foo) {
+        return StringEscapeUtils.escapeJava(foo);
+    }
+
+    public Map<Integer, Object> q(String query) throws SQLException {
+        Map<Integer, Map<Integer, Object>> retValue = qm(query, 1, true);
+        return retValue.get(1);
+    }
+    /*
+    public Map<Integer, Object> q(String query) throws SQLException {
+        query = StringEscapeUtils.unescapeJava(query);
+        System.out.println("running query:\n"+query);
+        //Connection conn = DriverManager.getConnection(connectString, user, password);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Map<Integer, Object> retValue = new HashMap<Integer, Object>();
+        try {
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            boolean haveBeenHereBefore = false;
+            while (rs.next()) {
+                if (haveBeenHereBefore) throw new RuntimeException("more than one row returned from query: "+query);
+                for (int i = 1 ; i <= columnsNumber ; i++)
+                    retValue.put(i, rs.getObject(i));                                    
+                haveBeenHereBefore = true;
+            }
+            return retValue;
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(ps);
+            //JdbcUtils.closeConnection(conn);
+        }
+    }*/
+
+    public Map<Integer, Map<Integer, Object>> qm(String query, int maxNumOfRows, boolean assertNoMore) throws SQLException {
+        query = StringEscapeUtils.unescapeJava(query);
+        System.out.println("running query:\n"+query);
+        //Connection conn = DriverManager.getConnection(connectString, user, password);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Map<Integer, Map<Integer, Object>> retValue = new HashMap<Integer, Map<Integer, Object>>();
+        try {
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            int rowNum = 0 ;    
+            while (rs.next()) {
+                Map<Integer, Object> insideRetValue = new HashMap<Integer, Object>();
+                if (++rowNum > maxNumOfRows) {
+                    if (assertNoMore)
+                        throw new RuntimeException(String.format("more than the specified max number of rows (%d) returned from query:\n %s", maxNumOfRows, query));
+                    else
+                        break;
+                }
+                for (int i = 1 ; i <= columnsNumber ; i++)
+                    insideRetValue.put(i, rs.getObject(i));
+                retValue.put(rowNum, insideRetValue);
+            }
+            return retValue;
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(ps);
+            //JdbcUtils.closeConnection(conn);
+        }
+    }
+}
