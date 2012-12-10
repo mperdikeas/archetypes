db3604a5b71a20c369cee45d7b658fe2433e472e
mperdikeas <mperdikeas@gmail.com>
2012-12-10 12:27:19 +0200

    JASPER # 008 # 20 : Handling dates in the script

---
+++ jasper-reports/008-20-use-of-dates/src/ExtendedHashMap.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ExtendedHashMap extends HashMap<Integer, Object> {
+
+    public Map<String, Object> internal = new HashMap<String, Object>();
+    public Object f(String key) {
+        return internal.get(key);
+    }
+
+    public Object put(Integer key, String keys, Object value) {
+        // System.out.println(String.format("ExtendedHashMap: adding %d, %s -> %s", key, keys.toUpperCase(), value));
+        Object retValue = super.put(key, value);
+        internal.put(keys.toUpperCase(), value);
+        return retValue;
+    }
+
+    public static void main (String args[]) {
+        System.out.println("boo");
+        ExtendedHashMap fm = new ExtendedHashMap();
+        fm.put(1, "one", "The integer known as 'one'");
+        System.out.println(fm.get(1));
+        System.out.println(fm.f("one"));
+    }
+}
