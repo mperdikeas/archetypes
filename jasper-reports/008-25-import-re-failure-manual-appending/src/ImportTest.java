66f0f29f59f9ca271921e3a83a9b848473d36e24
mperdikeas <mperdikeas@gmail.com>
2012-12-17 20:35:20 +0200

    JASPER-REPORTS # 008 # 25 : manually appending the datetime.py script seems to work! (but will it work on a web context?)

---
+++ jasper-reports/008-25-import-re-failure-manual-appending/src/ImportTest.java
@@ -0,0 +1,26 @@
+import java.io.StringWriter;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.python.core.*;
+import org.python.util.PythonInterpreter;
+
+
+public class ImportTest {
+    public static void main(String[] args) throws Exception {
+        ImportTest it = new ImportTest();
+        it.work();
+    }
+    public void work() throws IOException {
+            PythonInterpreter pi = new PythonInterpreter();
+            PyObject result = null;
+            String script = "import datetime"; // "import re" exemplifies the exact same behaviour
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(getClass().getResourceAsStream("datetime.py"), writer, "US-ASCII");
+            String preample = writer.toString();
+            PyCode code = pi.compile(preample+"\n"+script); 
+            result = pi.eval(code);
+    }	
+
+
+}
