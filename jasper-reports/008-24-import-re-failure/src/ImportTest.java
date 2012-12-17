4877b230e5ca306aaeef23b92078cf0f2195fa92
mperdikeas <mperdikeas@gmail.com>
2012-12-17 19:47:32 +0200

    JASPER-REPORTS # 008 # 24 : same thing with 'import datetime' (as with 'import re')

--- jasper-reports/008-24-import-re-failure/src/ImportTest.java
+++ jasper-reports/008-24-import-re-failure/src/ImportTest.java
@@ -7,7 +7,7 @@ public class ImportTest {
             PythonInterpreter pi = new PythonInterpreter();
             PyObject result = null;
 
-            PyCode code = pi.compile("import re");
+            PyCode code = pi.compile("import datetime"); // "import re" exemplifies the exact same behaviour
             result = pi.eval(code);
     }	
 
