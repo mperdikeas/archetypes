0ae212a0ae2f2c5b98076d70c18a5f6cce7d77b0
mperdikeas <mperdikeas@gmail.com>
2013-01-18 18:38:47 +0200

    JAX-RS # 001 # 003 : gsis various attempts to master JAX-WS (actually I noticed that 'jax-rs' in the folder name is a misnomer)

---
+++ jax-rs/001-03-gsis-ws_wsdl_my_own_console_ws/export-jboss.sh
@@ -0,0 +1,1 @@
+export JBOSS_HOME=/home/mperdikeas/git-jboss-eap-6.0/jboss-eap-6.0/
---
+++ jax-rs/001-03-gsis-ws_wsdl_my_own_console_ws/src/FooMain.java
@@ -0,0 +1,28 @@
+import gr.gsis.rgwsbasstoixn.rgwsbasstoixn.RgWsBasStoixN_Service;
+import gr.gsis.rgwsbasstoixn.rgwsbasstoixn.RgWsBasStoixN;
+	
+
+
+
+public class FooMain {
+
+
+    public static void main (String args[]) {
+        System.out.println( "about to use the gsis web service" ) ;
+
+        RgWsBasStoixN_Service 
+
+            /***
+	WSNPProxy proxy = new WSNPProxy("https://www1.gsis.gr/wsgsis/RgWsBasStoixN/RgWsBasStoixNSoapHttpPort");
+
+	String version = proxy.rgWsBasStoixNVersionInfo();
+        System.out.println("Service Version: "+version);
+
+        String afm = "047787907";
+        WSNPResponse response  = proxy.rgWsBasStoixN(afm);
+        System.out.println (response.pBasStoixNRecOut.getOnomasia()); ***/
+
+
+    }
+
+}
