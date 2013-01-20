1b0cdf88ad2c3b6976caf5250dd4a07a95e75fc4
mperdikeas <mperdikeas@gmail.com>
2013-01-20 22:32:22 +0200

    JAX-WS # 001 # 05 : trying to narrow-down the ClassNotFound problem

---
+++ jax-ws/001-05-gsis_ws_wsdl_using-java6-trying_to_narrow_downCNFE/src/FooMain.java
@@ -0,0 +1,6 @@
+public class FooMain {
+
+    public static void main(String args[]) throws ClassNotFoundException {
+        Class klass = Class.forName("com.sun.xml.internal.ws.spi.ProviderImpl");
+    }
+}
