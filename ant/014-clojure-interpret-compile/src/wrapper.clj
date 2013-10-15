d24712356c1565e7eea3c28a1e91b73603bb9268
mperdikeas <mperdikeas@gmail.com>
2013-10-15 21:02:05 +0200

    ANT :: 014 :: demonstrating how to use command line arguments for the simple interpreter case

--- ant/014-clojure-interpret-compile/src/wrapper.clj
+++ ant/014-clojure-interpret-compile/src/wrapper.clj
@@ -1,4 +1,21 @@
 (ns not_used
+  (:use [clojure.tools.cli :only (cli)])
   (:use [bar.zar.Foo :only (-main)]))
 
-(-main "-m" "this is the interpreter message (only three times)" "-n" 3)
+
+(def opts
+  (let [opts-args-banner
+        (cli *command-line-args*
+             ["-h" "--help" "Show help" :flag true :default false]
+             ["-n" "--number-of-printout" "number of times to print messages" :default 1]
+             ["-m" "--message" "REQUIRED: message to print"]
+             ["-v" "--verbose" "verbose"])]
+    (first opts-args-banner)))
+
+
+(def simpleWay false)
+
+(if simpleWay
+  (-main "-m" "this is the simple interpreter message (only three times)" "-n" 3)
+  (-main "-m" (:message opts) "-n" (Integer/parseInt (:number-of-printout opts))))
+  
