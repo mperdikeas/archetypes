c92ed16a58e432e61328459c2cb18ce6771e7b4c
mperdikeas <mperdikeas@gmail.com>
2016-04-18 19:26:44 +0200

    foo

--- babel-webpack-node-react/00-simple-use-of-babel-server-side-node-only-no-webpack/es6/index.js
+++ babel-webpack-node-react/00-simple-use-of-babel-server-side-node-only-no-webpack/es6/index.js
@@ -4,5 +4,9 @@
 //
 
 require('babel-polyfill'); // this is important as Babel only transforms syntax (e.g. arrow functions)
-                           // so you need this in order to support new globals or (in my experience) well-known Symbols
+// so you need this in order to support new globals or (in my experience) well-known Symbols, e.g. the following:
+//
+//     console.log(Object[Symbol.hasInstance]);
+//
+// ... will print 'undefined' without the the babel-polyfill being required.
 require('./app.js');
