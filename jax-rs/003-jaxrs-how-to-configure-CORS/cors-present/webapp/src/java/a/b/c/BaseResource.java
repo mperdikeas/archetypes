39a00ff529d7abfc173fdc3cf87dcacea03b0a05
mperdikeas <mperdikeas@gmail.com>
2016-05-23 12:33:07 +0300

    archetype of how to configure HTTP access control CORS with Jersey jax-rs

---
+++ jax-rs/003-jaxrs-how-to-configure-CORS/cors-present/webapp/src/java/a/b/c/BaseResource.java
@@ -0,0 +1,44 @@
+package a.b.c;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public abstract class BaseResource {
+
+
+    private static List<DataSource> pastDataSources = new ArrayList<>();
+
+    protected DataSource getDataSource() {
+        try {
+            InitialContext cxt = new InitialContext();
+            if ( cxt == null ) {
+                throw new RuntimeException("Uh oh -- no context!");
+            }
+            final String DS_LOOKUP_NAME = "java:/comp/env/jdbc/postgres/silly-test-database";
+            DataSource ds = (DataSource) cxt.lookup( DS_LOOKUP_NAME );
+            // this shows that the DataSource object is very likely always the same:
+            System.out.printf("Identity hashCode of DataSource object is [%s]\n"
+                              , System.identityHashCode(ds));
+            // this proves that the DataSource object is always the same:
+            if (pastDataSources.isEmpty())
+                pastDataSources.add(ds);
+            else {
+                if (!pastDataSources.contains(ds))
+                    throw new RuntimeException(String.format("%d datasources exist and none is the newly created one"
+                                                             , pastDataSources.size()));
+            }
+            if ( ds == null ) {
+                throw new RuntimeException(String.format("Data source [%s] not found!"
+                                                         , DS_LOOKUP_NAME));
+            } else
+                return ds;
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
